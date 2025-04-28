bfd417299622bd190e72193fb4900e79aba8db4f
nirmalmahen-ideas2it <alagunirmal.mahendran@ideas2it.com>
2025-04-28 11:47:23 +0530

    Remove unused method for retrieving all users from UserCacheService

--- src/main/java/com/sample/service/caching/UserCacheService.java
+++ src/main/java/com/sample/service/caching/UserCacheService.java
@@ -2,14 +2,10 @@ package com.sample.service.caching;
 
 import com.sample.service.dto.UserInfo;
 
-import java.util.List;
-
 public interface UserCacheService {
     void saveUser(UserInfo user);
 
     UserInfo getUser(Long id);
-
-    List<UserInfo> getAllUsers();
 
     void deleteUser(Long id);
 }
