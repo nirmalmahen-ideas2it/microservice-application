e5a5c07bfbcecad76926ad83a13a7a6ab1a3348c
nirmalmahen-ideas2it <alagunirmal.mahendran@ideas2it.com>
2025-04-30 13:27:28 +0530

    Add Keycloak setup instructions and update OpenAPI configuration for dynamic URLs

--- src/main/java/com/sample/config/OpenApiConfig.java
+++ src/main/java/com/sample/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,22 +13,26 @@ public class OpenApiConfig {
 
     private static final String SECURITY_SCHEME_NAME = "Keycloak";
 
+    @Value("${oauth.authUri}")
+    private String authorizationUrl;
+
+    @Value("${oauth.tokenUri}")
+    private String tokenUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "keycloak";
-
         return new OpenAPI()
             .info(new Info().title("User and Role Module").version("v1"))
-            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-            .components(new Components().addSecuritySchemes(securitySchemeName,
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+            .components(new Components().addSecuritySchemes(SECURITY_SCHEME_NAME,
                 new SecurityScheme()
                     .type(SecurityScheme.Type.OAUTH2)
                     .scheme("bearer")
                     .bearerFormat("JWT")
                     .flows(new OAuthFlows()
                         .authorizationCode(new OAuthFlow()
-                            .authorizationUrl("http://localhost:7070/realms/usersApp-relam/protocol/openid-connect/auth")
-                            .tokenUrl("http://localhost:7070/realms/usersApp-relam/protocol/openid-connect/token")
+                            .authorizationUrl(authorizationUrl)
+                            .tokenUrl(tokenUrl)
                             .scopes(new Scopes().addString("openid", "OpenID Connect scope"))
                         )
                     )
